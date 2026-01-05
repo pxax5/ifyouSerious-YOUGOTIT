a6bf2b065ae95b8d61e4edb2573732208226b21c
pxax5 <jiraphat9298@gmail.com>
2026-01-05 12:29:23 +0700

    lab11_1 is complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    string input;
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    // รอ Enter 3 ครั้ง
+    for (int i = 0; i < 3; i++) {
+        getline(cin, input);
+    }
+
+    // สุ่มเกรด
+    srand(time(0));
+    int randIndex = rand() % 9;
+
+    cout << "You will get " << grades[randIndex]
+         << " in this 261102." << endl;
+
+    return 0;
+}
